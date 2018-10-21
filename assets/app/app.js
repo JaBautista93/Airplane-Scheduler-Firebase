235e545cf18c269db640232d9b902a94d4cfc3fa
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-10-21 15:26:35 -0500

    updated javascript to work

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,106 +1,137 @@
- // Initialize Firebase
- var config = {
-     apiKey: "AIzaSyDqT1b88zOaaeew2D1j1yNDjMNVdyHQrAc",
-     authDomain: "flight-tracker-b1c2b.firebaseapp.com",
-     databaseURL: "https://flight-tracker-b1c2b.firebaseio.com",
-     projectId: "flight-tracker-b1c2b",
-     storageBucket: "flight-tracker-b1c2b.appspot.com",
-     messagingSenderId: "664708434965"
- };
- firebase.initializeApp(config);
+var isNameEntered = false;
+var update;
 
- // Create a variable to reference the database
- var database = firebase.database();
+src = "https://www.gstatic.com/firebasejs/5.5.2/firebase.js"
 
- // Note remember to create these same variables in Firebase!
- var flight = "";
- var destination = "";
- var takeoff = "";
- var frequency = "";
- var nextflight = "";
- var minute = "";
- var tMinutesTillPlane = "";
- var nextFlight = "";
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDqT1b88zOaaeew2D1j1yNDjMNVdyHQrAc",
+    authDomain: "flight-tracker-b1c2b.firebaseapp.com",
+    databaseURL: "https://flight-tracker-b1c2b.firebaseio.com",
+    projectId: "flight-tracker-b1c2b",
+    storageBucket: "flight-tracker-b1c2b.appspot.com",
+    messagingSenderId: "664708434965"
+};
+firebase.initializeApp(config);
 
- // Capture Button Click
- $("#add-flight").on("click", function (event) {
-     // Don't refresh the page!
-     event.preventDefault();
+var dataRef = firebase.database();
 
-     // Get inputs
-     flight = $("#flight-input").val().trim();
-     destination = $("#destination-input").val().trim();
-     takeoff = $("#time-input").val().trim();
-     frequency = $("#frequency-input").val().trim();
 
-     time = $("#time-input").val().trim();
-     console.log(time)
-     frequency = $("#frequency-input").val().trim();
-     console.log(frequency)
-     // First Time (pushed back 1 year to make sure it comes before current time)
-     var firstTimeConverted = moment(time, "HH:mm").subtract(1, "years");
-     console.log(firstTimeConverted);
+$("#timeDisplay").html(moment().format("hh:mm: A"))
 
-     // Current Time
-     var currentTime = moment();
-     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+// Capture Button Click
+$("#add-train-info").on("click", function (event) {
+  event.preventDefault();
+  // gets train info
+  var trainName = $("#trainName").val().trim();
+  var destination = $("#destination").val().trim();
+  var frequency = $("#frequency").val().trim();
+  var firstTime = $("#first-train-time").val().trim();
 
-     // Difference between the times
-     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-     console.log("DIFFERENCE IN TIME: " + diffTime);
 
-     // Time apart (remainder)
-     var tRemainder = diffTime % frequency;
-     console.log(tRemainder);
+  if (trainName !== "") {
 
-     // Minute Until flight
-     tMinutesTillPlane = frequency - tRemainder;
-     console.log("MINUTES TILL FLIGHT: " + tMinutesTillPlane);
+    // object to hold train data
+    var newTrainInfo = {
 
-     // Next flight
-     nextFlight = moment().add(tMinutesTillPlane, "minutes");
-     console.log("ARRIVAL TIME: " + moment(nextFlight).format("hh:mm"));
+      nameOfTrain: trainName,
+      nameOfDestination: destination,
+      nameOfFrequency: frequency,
+      nameOFfirstTime: firstTime,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+
+    // push the data into database
+    dataRef.ref().push(newTrainInfo);
+
+  } else {
+    dataRef.ref().push(newTrainInfo);
+    alert("enter the train name")
+  }
+
+  // clears all the textboxes
+  $("#trainName").val("")
+  $("#destination").val("")
+  $("#frequency").val("")
+  $("#first-train-time").val("")
+
+
+});
 
 
 
-     // So this kept causing an error, it just stopped worked at some point and I am not sure why I so took it out
-     // Print the initial data to the console.
-     //console.log(snapshot.val());
-
-     // Log the value of the various properties
-     //  console.log(snapshot.val().flight);
-     //  console.log(snapshot.val().destination);
-     //  console.log(snapshot.val().takeoff);
-     //  console.log(snapshot.val().frequency);
-     //  console.log(snapshot.val().tMinutesTillPlane);
-     //  console.log(snapshot.val().nextFlight);
+snapshot();
+setInterval(snapshot, 60000)
 
 
-     // Code in the logic for storing and retrieving the most recent user.
-     database.ref().push({
-         flight: flight,
-         destination: destination,
-         takeoff: takeoff,
-         frequency: frequency,
-         tMinutesTillPlane: tMinutesTillPlane,
-         nextFlight: nextFlight,
-         dateAdded: firebase.database.ServerValue.TIMESTAMP
-     });
+function snapshot() {
+  // empty the contents of the body tag
+  $("#train-info-display > tbody").empty(),
 
- });
- // Firebase is always watching for changes to the data.
- // When changes occurs it will print them to console and html
- database.ref().on("child_added", function (snapshot) {
+    // displays the time
+    $("#timeDisplay").html(moment().format("llll")).css('color','aqua')
+ 
+  dataRef.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
 
-     // Change the HTML
-     $("#flight-display").after(snapshot.val().flight);
-     $("#desination-display").after(snapshot.val().destination);
-     $("#frequency-display").after(snapshot.val().takeoff);
-     $("#minute-display").append(tMinutesTillPlane);
-     $("#nextarriaval-display").append(nextFlight);
+    //train info
 
-     // Handle the errors
- }, function (errorObject) {
-     console.log("Errors handled: " + errorObject.code);
- });
+    var trainName = childSnapshot.val().nameOfTrain;
+    var destination = childSnapshot.val().nameOfDestination;
+    var frequency = childSnapshot.val().nameOfFrequency;
+    var firstTime = childSnapshot.val().nameOFfirstTime;
+
+    var time_converted = moment(firstTime, "HH:mm").subtract(1, "years")
+    console.log(time_converted)
+
+    var child_key = childSnapshot.key; // variable to get child key
+
+    // diffence in minutes between the times
+    var minutes_difference = moment().diff(moment(time_converted), "minutes");
+    console.log(minutes_difference)
+
+    // minutes from the time of arrival
+    var time_remaining = frequency - (minutes_difference % frequency);
+    console.log(" The time remainig before arrival time is : " + time_remaining);
+
+    // next train arrival time
+    var nextArrival = moment().add(time_remaining, "minutes").format("hh:mm:A") // gets the arrival time in minutes
+    // var arrivalTime = moment(nextArrival).format("hh:mm:A") // gets the arrival time in this normal time formal
+    // console.log( " The current time is : " + current_time)
+    console.log(" the next arrival time is : " + nextArrival)
+
+    // delete button dynamically created
+    var deleteRow = $("<button>").addClass("btn btn-danger delete").text("delete").attr("data-key", child_key)
+    $(deleteRow).on("click", function () {
+      console.log("click me")
+      var keyref = $(this).attr("data-key");
+      dataRef.ref().child(keyref).remove();
+      window.location.reload();
+    })
+
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(destination),
+      $("<td>").text(frequency),
+      $("<td>").text(nextArrival),
+      $("<td>").text(time_remaining),
+      $("<td>").append(deleteRow),
+
+    );
+    // Append the new row to the table
+    $("#train-info-display > tbody").append(newRow),
+
+
+      function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+
+
+
+  });
+
+}
+
 
